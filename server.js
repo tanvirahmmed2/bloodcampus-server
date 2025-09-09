be9b6ea86ba07696f9c78a87be2a69c3f96f9160
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-09 19:17:45 +0600

    update

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-require('dotenv').config()
-const app= require('./src/app')
-const PORT= process.env.PORT || 4000
+require('dotenv').config();
+const app = require('./src/app');
+const mongoose = require('mongoose');
 
+const PORT = process.env.PORT || 4000;
+const MONGO_URL = process.env.MONGO_URL;
 
-app.listen(PORT, (error)=>{
-    if(!error){
-        console.log(`Server is running at http://localhost:${PORT}`)
-    }else{
-        console.log(error)
+const connectMongo = async () => {
+    try {
+        await mongoose.connect(MONGO_URL);
+        console.log("✅ MongoDB Database Connected");
+
+        app.listen(PORT, () => {
+            console.log(` Server is running at http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error(" MongoDB Connection Failed:", error.message);
+        process.exit(1);
     }
-})
+};
 
-
+connectMongo();
