94df3bb694a6b3382b0f5902b52c43072187b785
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-09 19:19:00 +0600

    update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const MONGO_URL = process.env.MONGO_URL;
 const connectMongo = async () => {
     try {
         await mongoose.connect(MONGO_URL);
-        console.log("✅ MongoDB Database Connected");
+        console.log(" MongoDB Database Connected");
 
         app.listen(PORT, () => {
             console.log(` Server is running at http://localhost:${PORT}`);
