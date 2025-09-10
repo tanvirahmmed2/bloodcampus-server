45afa11a1088610deb79ea05e611ad019ab67690
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-10 23:44:16 +0600

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,10 +9,10 @@ const User = require("../models/user.model");
 // Get all users (without passwords)
 const getUser = async (req, res) => {
   try {
-    const users = await User.find({}, "-password"); // exclude password field
-    return res.status(200).send(users);
+    const users = await User.find({}, "-password"); // exclude password
+    return res.status(200).json(users); // make sure this is an array
   } catch (error) {
-    console.error("GetUser error:", error);
+    console.error(error);
     return res.status(500).json({
       success: false,
       message: "Something went wrong",
