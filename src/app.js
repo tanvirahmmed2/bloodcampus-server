77275515d8431c5a53ae9468a255cc933841fac3
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-10 23:28:50 +0600

    update

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
-const express= require('express')
-const cors= require('cors')
+const express = require('express')
+const cors = require('cors')
 const userRouter = require('./routers/user.router')
 
 
 
 
-const app= express()
-app.use(express.urlencoded({extended: true}))
+const app = express()
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(cors({
-    origin: 'https://bloodcampus.netlify.app/'
+    origin: 'https://bloodcampus.netlify.app/',
+    methods: ["GET", "POST"],
+    credentials: true
 }))
 
 app.use('/api/user', userRouter)
 
 
-module.exports= app
+module.exports = app
