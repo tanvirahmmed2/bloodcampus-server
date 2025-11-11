c06b9cd64ee6b176540613a340dc14990f6780bb
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-11 16:38:23 +0600

    update

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -26,23 +26,68 @@ const getMessages = async (req, res) => {
 }
 
 
-const newMessage=async (req,res) => {
+const newMessage = async (req, res) => {
     try {
-        
+        const { name, message, email, subject } = req.body
+        if (!name || !message || !email || !subject) {
+            return res.status(400).send({
+                success: false,
+                message: 'All fields are required'
+            });
+        }
+        const existMessage = await Message.findOne({ email: email })
+        if (existMessage) {
+            return res.status(400).send({
+                success: false,
+                message: 'One message already in review'
+            });
+        }
+        const newMessage = new Message({ name, message, email, subject })
+        await newMessage.save()
+        return res.status(200).send({
+            success: true,
+            message: 'Successfully sent message'
+        });
     } catch (error) {
-        
+        return res.status(500).send({
+            success: false,
+            message: 'Failed to send message',
+            error: error.message
+        });
     }
-    
+
 }
 
 
-const deleteMessage=async (req,res) => {
+const deleteMessage = async (req, res) => {
     try {
-        
+        const { id } = req.body
+        if (!id) {
+            return res.status(400).send({
+                success: false,
+                message: 'Message id not found'
+            });
+        }
+        const message = await Message.findById(id)
+        if (!message) {
+            return res.status(400).send({
+                success: false,
+                message: 'No message found with this id'
+            });
+        }
+        await Message.findByIdAndDelete(id)
+        return res.status(200).send({
+            success: true,
+            message: 'successfully deleted message'
+        });
     } catch (error) {
-        
+        return res.status(500).send({
+            success: false,
+            message: 'Failed to send message',
+            error: error.message
+        });
     }
-    
+
 }
 
 module.exports = {
