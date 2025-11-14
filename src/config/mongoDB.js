b167237fe0c44dc7d2d601be72f89981f71a44af
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-15 00:03:57 +0600

    update

--- src/config/mongoDB.js
+++ src/config/mongoDB.js
@@ -1,15 +1,18 @@
-const mongoose= require('mongoose')
-const { MONGO_URL } = require('./secure')
+const mongoose = require('mongoose');
+const { MONGO_URL } = require('./secure');
 
-const connectDB= async () => {
-    try {
-        await mongoose.connect(MONGO_URL)
-        console.log('DataBase connected successfully')
-    } catch (error) {
-        console.log(error)
-        
-    }
-    
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-module.exports= connectDB
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('Database connection failed:', error.message);
+    process.exit(1); 
+  }
+};
+
+module.exports = connectDB;
