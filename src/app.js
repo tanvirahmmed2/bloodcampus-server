1de802ff1473c8dfa9ead3d364aea406a237b2d1
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-15 17:48:04 +0600

    update

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser())
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://bloodcampus.netlify.app',
     credentials: true,
 }));
 
